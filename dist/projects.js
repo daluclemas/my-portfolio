7fb4e45757c79ca89da70d552d5c238b5848160f
daluclemas <chidaluanukam@gmail.com>
2022-07-21 13:29:34 +0100

    dark theme for projects page

--- dist/projects.js
+++ dist/projects.js
@@ -1,7 +1,14 @@
 "use strict";
 const goback = document.querySelector('.go-back');
+const togglebtn = document.querySelector('.theme-toggle');
+const toggleicon = document.querySelector('.fa-moon');
 goback.addEventListener('click', (e) => {
     if (e.target.classList.contains('fa-angle-left')) {
         window.history.back();
     }
+});
+togglebtn.addEventListener('click', (e) => {
+    toggleicon.classList.toggle('fa-sun');
+    togglebtn.classList.toggle('active-toggle');
+    document.documentElement.classList.toggle('dark-mode');
 });
--- src/projects.ts
+++ src/projects.ts
@@ -1,7 +1,18 @@
 const goback = document.querySelector('.go-back') as HTMLDivElement;
 
+const togglebtn = document.querySelector('.theme-toggle') as HTMLDivElement;
+
+const toggleicon = document.querySelector('.fa-moon') as HTMLElement;
+
 goback.addEventListener('click', (e: Event | any) => {
   if (e.target.classList.contains('fa-angle-left')) {
     window.history.back();
   }
+});
+
+togglebtn.addEventListener('click', (e: Event | any) => {
+
+  toggleicon.classList.toggle('fa-sun')
+  togglebtn.classList.toggle('active-toggle')
+  document.documentElement.classList.toggle('dark-mode')
 });
